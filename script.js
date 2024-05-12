a2f6165d9cf96da612010833f629e3a610050c0c
KevalTurakhia <92883323+KevalTurakhia@users.noreply.github.com>
2024-05-12 16:43:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,84 @@
-const inputtdl = document.querySelector('.textarea')
-const buttontdl = document.querySelector('.buttoninput')
-const listtdl = document.querySelector('.todolist')
-
-function clickButton(e) {
-    e.preventDefault()
-    addTodo()
-}
-
-// adding todoList
-function addTodo() {
-    const itemall = document.createElement('div')
-    itemall.classList.add('itemall')
-
-    const item = document.createElement('p')
-    item.classList.add('item')
-    item.innerText = inputtdl.value
-    itemall.appendChild(item)
-
-    if (inputtdl.value === '') return
-
-    const checkbutton = document.createElement("button")
-    checkbutton.innerHTML = '<i class="fa-solid fa-check"></i>'
-    checkbutton.classList.add("check-button")
-    itemall.appendChild(checkbutton)
-
-    const trashbutton = document.createElement("button")
-    trashbutton.innerHTML = '<i class="fa-solid fa-trash"></i>'
-    trashbutton.classList.add("trash-button")
-    itemall.appendChild(trashbutton)
-
-    listtdl.appendChild(itemall)
-    inputtdl.value = ''
-}
-
-// checking and delete todoList 
-function okdel(e) {
-    const item = e.target
-
-    // check
-    if (item.classList[0] === 'check-button') {
-        const todolist = item.parentElement
-        todolist.classList.toggle('checklist')
+$(document).ready(function () {
+  // Scroll down sticky navbar script start
+  $(window).scroll(function () {
+    if (this.scrollY > 20) {
+      $(".navbar").addClass("sticky");
+    } else {
+      $(".navbar").removeClass("sticky");
     }
+    // Scroll down sticky navbar script end
 
-    // delete
-    if (item.classList[0] === 'trash-button') {
-        const todolist = item.parentElement
-        todolist.remove()
+    // Scroll up Button script start
+    if (this.scrollY > 500) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
     }
-}
+    // Scroll up Button script end
 
-buttontdl.addEventListener('click', clickButton)
-listtdl.addEventListener('click', okdel)
+    // Fade In & Fade Out Elements on Scroll script start
+    $(".fadein").each(function (i) {
+      var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+      if (bottom_of_window > bottom_of_element) {
+        $(this).addClass("showme");
+      }
+      if (bottom_of_window < bottom_of_element) {
+        $(this).removeClass("showme");
+      }
+      // Fade In & Fade Out Elements on Scroll script end
+    });
+  });
+
+  // Scroll up Button script start
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 });
+  });
+  // Scroll up Button script end
+
+  // Typing animation script start
+  var typed = new Typed(".typing", {
+    strings: ["React.js Developer", "Front End Developer", "Youtuber", "Teacher"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
+
+  var typed = new Typed(".typing2", {
+    strings: ["React.js Developer", "Front End Developer", "Youtuber", "Teacher"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
+
+  // toggle menu/navbar script start
+  $(".menu-btn").click(function () {
+    $(".navbar .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+  });
+  // toggle menu/navbar script end
+
+  // owl carousel script start
+  $(".carousel").owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      600: {
+        items: 2,
+        nav: false,
+      },
+      1000: {
+        items: 3,
+        nav: false,
+      },
+    },
+  });
+  // owl carousel script end
+});
